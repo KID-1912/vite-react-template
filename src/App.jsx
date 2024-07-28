8b4abf0b617e004ec8ea42ca48ed183ec7048e16
page <heyutongxue@gmail.com>
2024-07-28 23:44:26 +0800

    feat:#root class style

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,6 @@ function App() {
   return (
     <>
       <h1>Hello World</h1>
-      <h2>{count}</h2>
-      <button type="button" onClick={() => setCount(count + 1)}>
-        尝试
-      </button>
     </>
   );
 }
